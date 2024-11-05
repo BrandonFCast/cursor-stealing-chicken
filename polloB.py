37e88e32c8f932a76d9cad3351cb57b19f1ca202
Brandon BC <brand.bc0@gmail.com>
2024-11-04 19:56:45 -0600

    language corrections

--- polloB.py
+++ polloB.py
@@ -17,7 +17,7 @@ def resource_path(relative_path): # This function reads files as requested by th
 vx = 10 # view x
 vy = 10 # view y
 x, y = pyautogui.position() # mouse position
-ventana = tk.Tk()
+window = tk.Tk()
 pyautogui.FAILSAFE = False
 
 # GUI
@@ -26,7 +26,7 @@ bigPetLImg = petLImg.zoom(4, 4)
 petRImg = tk.PhotoImage(file=resource_path("assets/polloR.png"))
 bigPetRImg = petRImg.zoom(4, 4)
 
-petLbl = tk.Label(ventana, image=bigPetLImg, bg='black')
+petLbl = tk.Label(window, image=bigPetLImg, bg='black')
 petLbl.pack()
 
 # constants
@@ -37,32 +37,32 @@ cursor_in_window = False # if cursor is on the chicken it will be True
 after_id = 0
 
 def lose():
-    global ventana
+    global window
     pyautogui.moveTo(vx + WINDOW_SIZE / 2, vy + WINDOW_SIZE / 2, duration=0)
-    ventana.after(10, lose)
+    window.after(10, lose)
 
 def on_enter(event):
-    global after_id, ventana, vy, vx
-    ventana.after_cancel(after_id)
+    global after_id, window, vy, vx
+    window.after_cancel(after_id)
     lose()
 
 def createWindow():
 
     # preparing the window
-    global ventana, vx, vy, x, y
-    ventana.configure(background='black', cursor="none")
-    ventana.attributes('-transparentcolor', 'black')
-    ventana.attributes('-topmost', True)
-    ventana.geometry(f"{WINDOW_SIZE}x{WINDOW_SIZE}+{vx}+{vy}")
-    ventana.bind('<F3>', lambda e: ventana.destroy())
-    ventana.overrideredirect(True)
-    ventana.bind('<Enter>', on_enter)
+    global window, vx, vy, x, y
+    window.configure(background='black', cursor="none")
+    window.attributes('-transparentcolor', 'black')
+    window.attributes('-topmost', True)
+    window.geometry(f"{WINDOW_SIZE}x{WINDOW_SIZE}+{vx}+{vy}")
+    window.bind('<F3>', lambda e: window.destroy())
+    window.overrideredirect(True)
+    window.bind('<Enter>', on_enter)
 
     start()
-    ventana.mainloop()
+    window.mainloop()
 
 def start(): # This function starts the movement of the window
-    global x, y, vx, vy, ventana, petLbl, cursor_in_window, after_id
+    global x, y, vx, vy, window, petLbl, cursor_in_window, after_id
     speed = 1
     x, y = pyautogui.position()
     if (x > vx + WINDOW_SIZE / 2):
@@ -77,8 +77,8 @@ def start(): # This function starts the movement of the window
         vy += speed
     if (y < vy + WINDOW_SIZE / 2):
         vy -= speed
-    ventana.geometry(f"{WINDOW_SIZE}x{WINDOW_SIZE}+{int(vx)}+{int(vy)}")
-    after_id = ventana.after(20, start)
+    window.geometry(f"{WINDOW_SIZE}x{WINDOW_SIZE}+{int(vx)}+{int(vy)}")
+    after_id = window.after(20, start)
 
 time.sleep(10)
 createWindow()
